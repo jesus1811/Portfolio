edc3d36ae9e74a88b09d4c69756bc00da776d804
jesus1811 <jayarza1811@gmail.com>
2022-08-11 15:20:17 -0500

    ✅ add resources assets

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useContext } from "react";
+import { Link } from "react-scroll";
+import styles from "./styles.module.scss";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <section className={styles.container}>
+      <div className={styles.containerDiv}>
+        <input type="checkbox" id="check" className={styles.checkedNav} />
+        <Link to="inicio" smooth={true} className={styles.linkMain}>
+          JesuDev
+        </Link>
 
-export default Header
+        <label htmlFor="check" className="btnCheck">
+          <img src="menu.svg" alt="" className={styles.image} />
+        </label>
+        <nav className={styles.navigation}>
+          <Link to="about" smooth={true} className={styles.link}>
+            Sobre Mi
+          </Link>
+          <Link to="skill" smooth={true} className={styles.link}>
+            Skills
+          </Link>
+          <Link to="proyect" smooth={true} className={styles.link}>
+            Proyectos
+          </Link>
+          <Link to="contact" smooth={true} className={styles.link}>
+            Contactos
+          </Link>
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
