7378b613e6822111524c9f2aa56248f8c4295eea
jesus1811 <jayarza1811@gmail.com>
2022-08-14 18:06:44 -0500

    🛠  bug analytics

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer, Header, Layout } from "@/components";
 import { About, Banner, Projects, Skills } from "./components";
+import Script from "next/script";
 const Home = () => {
   return (
     <Layout title="Home | JesuDev" description="portfolio de jesudev">
@@ -10,6 +11,16 @@ const Home = () => {
       <Skills />
       <Projects />
       <Footer />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-3Z5SMJJQ81" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3Z5SMJJQ81');
+        `}
+      </Script>
     </Layout>
   );
 };
