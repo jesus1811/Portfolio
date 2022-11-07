ebe4a5330b1ed507caadee171b9cf1d0a0979294
jesus <jayarza1811@gmail.com>
2022-11-07 14:39:33 -0500

    fix url footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,19 +6,27 @@ const Footer = () => {
     <section className={`${styles.container} contact`}>
       <Title>Contactos</Title>
       <div className={styles.content}>
-        <a href="" target="_blank" rel="noreferrer">
+        <a
+          href="https://api.whatsapp.com/send?phone=936129604&text=Hola"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="whatsapp.svg" alt="" data-aos="fade-up" loading="lazy" />
           <p>WhatsApp</p>
         </a>
-        <a href="" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.linkedin.com/in/jesus-ayarza/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="linkedin.svg" alt="" data-aos="fade-up" loading="lazy" />
           <p>Linkedin</p>
         </a>
-        <a href="" target="_blank" rel="noreferrer">
+        {/* <a href="" target="_blank" rel="noreferrer">
           <img src="telegram.svg" alt="" data-aos="fade-up" loading="lazy" />
           <p>Telegram</p>
-        </a>
-        <a href="" target="_blank" rel="noreferrer">
+        </a> */}
+        <a href="https://github.com/jesus1811" target="_blank" rel="noreferrer">
           <img src="github.svg" alt="" data-aos="fade-up" loading="lazy" />
           <p>GitHub</p>
         </a>
