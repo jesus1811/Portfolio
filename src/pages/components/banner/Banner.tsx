eaf3d30f3354185d4d4c0d9df893a35e8a2e2ebc
jesus <jayarza1811@gmail.com>
2022-11-13 13:10:50 -0500

    fix rol banner component

--- src/pages/components/banner/Banner.tsx
+++ src/pages/components/banner/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
       <img src="/fondo.jpg" alt="setup fondo" />
       <div className={styles.datas}>
         <h1 data-aos="fade-up">Jesus Ayarza</h1>
-        <p data-aos="fade-up">React Front End Developer</p>
+        <p data-aos="fade-up">Front End Developer</p>
       </div>
     </section>
   );
